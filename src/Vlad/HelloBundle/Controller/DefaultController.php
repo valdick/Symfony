aedbe09d3c77d7acc84f4cc40845bc9a5be28101
vlad <valdick@inbox.ru>
2015-05-06 16:06:15 +0300

    Добавление фамилии

--- src/Vlad/HelloBundle/Controller/DefaultController.php
+++ src/Vlad/HelloBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name, $last_name)
     {
-        return $this->render('VladHelloBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('VladHelloBundle:Default:index.html.twig', array('name' => $name, 'last_name' => $last_name));
     }
 }
